6fe245dfb5fe1aa3a2c394c46f07639461152dbe
Enrique <quiquebd96@hotmail.com>
2019-08-23 06:00:47 -0500

    Updates

---
+++ precache-manifest.221e0d4e27e3adb1683b51843c0fcc80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56adcbe6b5a474e8a6ad1bd70507aca",
+    "url": "/chat.react.redux/index.html"
+  },
+  {
+    "revision": "1af8d51a582817e40a4c",
+    "url": "/chat.react.redux/static/css/main.260654ee.chunk.css"
+  },
+  {
+    "revision": "95954da0a0d374a01503",
+    "url": "/chat.react.redux/static/js/2.5edd2d4f.chunk.js"
+  },
+  {
+    "revision": "1af8d51a582817e40a4c",
+    "url": "/chat.react.redux/static/js/main.f4a5ccda.chunk.js"
+  },
+  {
+    "revision": "b240fef10cda53930f7f",
+    "url": "/chat.react.redux/static/js/runtime~main.c6ff287d.js"
+  },
+  {
+    "revision": "f9ad12e21c9d2ad97591b77c665876ef",
+    "url": "/chat.react.redux/static/media/jake.f9ad12e2.jpg"
+  },
+  {
+    "revision": "330b7d7884b9c538e0f469dc9b59ed56",
+    "url": "/chat.react.redux/static/media/logo.330b7d78.png"
+  }
+]);
